f2d5744202c095aa1b63bf1b43e5839af0f448fb
David Wickes <dave.wickes@gmail.com>
2014-09-22 18:23:24 +0100

    May well have a working current_user function

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -7,13 +7,13 @@ module.exports = function (app) {
 };
 
 router.get('/', function (req, res, next) {
-  db.Article.findAll().success(function (articles) {
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles,
-      id: req.session.user_id
-    });
-  });
+	db.Article.findAll().success(function (articles) {
+		res.render('index', {
+			title: 'Generator-Express MVC',
+			articles: articles,
+			id: req.session.user_id,
+		});
+	});
 });
 
 router.get('/articles', function (req, res, next) {
@@ -34,42 +34,51 @@ router.get('/articles/new', function (req, res, next) {
 });
 
 router.post('/articles', function (req, res, next) {
-  var text = req.body.text
-  var article = db.Article.build({title: req.body.title,
-  			  url: req.body.url,
-  			  text: req.body.text
-  			  });
-  article.save();
+	current_user(req, function(user) {
+		var article = db.Article.build({
+			title: req.body.title,
+			url: req.body.url,
+			text: req.body.text,
+			user: user
+		});
+		article.save().success(function(article) {console.log(article)});
+	});
 
-  db.Article.findAll().success(function (articles) {
-    res.render('articles', {
-      title: 'All them articles',
-      articles: articles
-    });
-  });
+	db.Article.findAll().success(function (articles) {
+		res.render('articles', {
+			title: 'All them articles',
+			articles: articles
+		});
+	});
 });
 
 router.get('/users/new', function (req, res, next) {
-  db.Article.findAll().success(function (articles) {
-    res.render('new_user', {
-      title: 'New User in Here',
+	db.Article.findAll().success(function (articles) {
+		res.render('new_user', {
+			title: 'New User in Here',
 		});
 	});
 });
 
 router.post('/users', function (req, res, next) {
-  
-  var user = db.User.build({name: req.body.name,
-  			  password: req.body.password
-  			  });
-  user.save().success(function(user){
-  	req.session.user_id = user.id;
-  });
-  db.User.findAll().success(function (users) {
-    res.render('users', {
-      title: 'Stalk the users',
-      users: users,
-      dog: req.session.user_id
+
+	var user = db.User.build({name: req.body.name,
+					password: req.body.password
+					});
+	user.save().success(function(user){
+		req.session.user_id = user.id;
+	});
+	db.User.findAll().success(function (users) {
+		res.render('users', {
+			title: 'Stalk the users',
+			users: users,
+			dog: req.session.user_id
 		});
 	});
 });
+
+var current_user = function (request, callback) {
+	db.User.find(request.session.user_id).success( function(user) {
+		callback(user);
+	});
+}
