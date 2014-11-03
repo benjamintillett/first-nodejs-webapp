c2048f9c64439a8a083b5a793d335ccf3f9eade8
benjamin tillett <benjamin.tillett@gmail.com>
2014-11-03 08:33:34 +0000

    add console.logs to config for debbugging database on heroku

--- config/config.js
+++ config/config.js
@@ -1,6 +1,9 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
     env = process.env.NODE_ENV || 'development';
+    console.log(process.env.DATABASE_URL);
+    console.log(env);
+    console.log("-----"*10);
 
 var config = {
   development: {
@@ -29,7 +32,7 @@ var config = {
       name: 'nodejs'
     },
     port: 3000,
-    db: process.env.DATABASE_URL
+    db: "postgres://rwfhojwqybjeio:AvqZ_lPG1s81AHTdkvsnP-6BY1@ec2-54-225-156-230.compute-1.amazonaws.com:5432/d89aqiq18r1np0"
     
   }
 };
