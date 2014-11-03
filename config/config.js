6df4244097c4509b4b9f6f7a5bf4e4cfbf70cc49
benjamin tillett <benjamin.tillett@gmail.com>
2014-11-03 08:43:00 +0000

    change production port to (process.env.PORT || 5000)

--- config/config.js
+++ config/config.js
@@ -31,7 +31,7 @@ var config = {
     app: {
       name: 'nodejs'
     },
-    port: 5000,
+    port: process.env.PORT || 5000
     db: "postgres://rwfhojwqybjeio:AvqZ_lPG1s81AHTdkvsnP-6BY1@ec2-54-225-156-230.compute-1.amazonaws.com:5432/d89aqiq18r1np0"
     
   }
